5aa16b4bfc9fc5c898a2f401a8ff896d2ed60f6a
Maxwell <msf9542@g.rit.edu>
2020-06-02 18:51:45 -0400

    basic functionality

--- linkedlist.py
+++ linkedlist.py
@@ -16,3 +16,27 @@ class LList:
         self.size = 0
         for arg in args:
             self.add(arg)
+
+    def add(self, element, index=0):
+        if not index:
+            self.root = Node(element, self.root)
+            self.size += 1
+        else:
+            try:
+                self.root = self.adder(
+                    self, element, self.root, index if index > 0 else index + len(self))
+            except:
+                IndexError(f'Cannot reach index {index}')
+
+    def adder(self, element, reference, index):
+        if index < 0:
+            raise IndexError()
+        if not index:
+            self.size += 1
+            return Node(element, reference)
+        if reference is None:
+            raise IndexError()
+        return self.adder(element, reference.rest, index - 1)
+
+    def __len__(self):
+        return self.size
